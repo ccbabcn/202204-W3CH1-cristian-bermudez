51ed5daaf726a040ee2a4b5b37a958730746a210
Cristian Bermúdez <ccbabcn@gmail.com>
2022-04-18 19:47:42 +0200

    installed and configured eslint

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "lines-between-class-members": [
+      "error",
+      "always",
+      { exceptAfterSingleLine: true },
+    ],
+    "no-plusplus": "off",
+    "no-restricted-syntax": "off",
+  },
+};
